11ddf649dbea2d66e9557f6dbda5845d5f1e1c73
Anthony Kimani <kimaniantoni@gmail.com>
2022-11-17 16:00:14 +0300

    finishes feature navigation
    - Adds a navigation bar and logo
    - Adds a Hero section background image and text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Prohealth Medical staffing </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
