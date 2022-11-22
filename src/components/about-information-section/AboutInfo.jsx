2321109942f953319c80de8f18a6bbbc7c042bb2
Anthony Kimani <kimaniantoni@gmail.com>
2022-11-22 17:57:45 +0300

    Adds an about page with an about section and footer

---
+++ src/components/about-information-section/AboutInfo.jsx
@@ -0,0 +1,17 @@
+const AboutInfo = () => {
+  return (
+    <div className="flex justify-around md:justify-end ">
+      <div className="flex flex-col items-center justify-around p-4 mt-[200px] max-w-[600px] text-white font-jost min-h-[200px] h-[100%] ">
+        <h1 className="text-4xl ">Our Story</h1>
+        <h3>
+          For nearly 2 years, Prohealth Medical Staffing has partnered with
+          healthcare providers by sourcing medical talent across multiple
+          disciplines to support their medical staffing and recruitment needs.
+          Today, our clinicians provide services across the state of Oregon.
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default AboutInfo;
